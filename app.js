c8feab97a0409eeb044bfa7059beabfac33b416f
dorian <wiwish2016@hotmail.com>
2026-04-28 11:08:57 -0600

    ac

--- app.js
+++ app.js
@@ -5,4 +5,15 @@ app.get('/', (req, res) => {
     res.send('Esta es una respuesta sencilla');
 });
 
+app.get('/suma', (req, res) => {
+    const a = Number(req.query.a);
+    const b = Number(req.query.b);
+
+    if (Number.isNaN(a) || Number.isNaN(b)) {
+        return res.status(400).send('Debes enviar numeros validos en a y b');
+    }
+
+    res.send(`El resultado de la suma es: ${a + b}`);
+});
+
 module.exports = app;
