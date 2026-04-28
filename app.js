201efcf0b11d86ffbf50995a291fed126ca06da3
dorian <wiwish2016@hotmail.com>
2026-04-28 11:23:32 -0600

    pruebas

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send('Esta es una respuesta sencilla');
+    res.send('Esta es una prueba sencilla');
 });
 
 app.get('/suma', (req, res) => {
